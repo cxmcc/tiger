0d1c52d406d54f6b2e383fb7fea6d107fa6f263c
Pierre CURTO <pierre.curto@gmail.com>
2017-05-20 21:02:56 +0200

    Use a bool instead of a func to work with endianness.
    benchmark                          old ns/op     new ns/op     delta
    BenchmarkHash8Bytes-4              450           338           -24.89%
    BenchmarkHash1K-4                  5330          4427          -16.94%
    BenchmarkHash8K-4                  39520         33118         -16.20%
    BenchmarkHash8BytesUnaligned-4     467           328           -29.76%
    BenchmarkHash1KUnaligned-4         5433          4417          -18.70%
    BenchmarkHash8KUnaligned-4         38640         32362         -16.25%
    
    benchmark                          old MB/s     new MB/s     speedup
    BenchmarkHash8Bytes-4              17.76        23.66        1.33x
    BenchmarkHash1K-4                  192.10       231.29       1.20x
    BenchmarkHash8K-4                  207.29       247.35       1.19x
    BenchmarkHash8BytesUnaligned-4     17.11        24.39        1.43x
    BenchmarkHash1KUnaligned-4         188.46       231.82       1.23x
    BenchmarkHash8KUnaligned-4         212.00       253.14       1.19x

--- compress.go
+++ compress.go
@@ -5,17 +5,12 @@ import (
 	"unsafe"
 )
 
-var castFunc func([]byte) uint64
+var littleEndian bool
 
 func init() {
 	x := uint32(0x04030201)
 	y := [4]byte{0x1, 0x2, 0x3, 0x4}
-	littleEndian := *(*[4]byte)(unsafe.Pointer(&x)) == y
-	if littleEndian {
-		castFunc = binary.LittleEndian.Uint64
-	} else {
-		castFunc = binary.BigEndian.Uint64
-	}
+	littleEndian = *(*[4]byte)(unsafe.Pointer(&x)) == y
 }
 
 func pass(a *uint64, b *uint64, c *uint64, x []uint64, mul uint64) {
@@ -61,15 +56,29 @@ func compress(d *digest, data []byte) {
 	bb := d.b
 	cc := d.c
 
-	x := []uint64{
-		castFunc(data[0:8]),
-		castFunc(data[8:16]),
-		castFunc(data[16:24]),
-		castFunc(data[24:32]),
-		castFunc(data[32:40]),
-		castFunc(data[40:48]),
-		castFunc(data[48:56]),
-		castFunc(data[56:64]),
+	var x []uint64
+	if littleEndian {
+		x = []uint64{
+			binary.LittleEndian.Uint64(data[0:8]),
+			binary.LittleEndian.Uint64(data[8:16]),
+			binary.LittleEndian.Uint64(data[16:24]),
+			binary.LittleEndian.Uint64(data[24:32]),
+			binary.LittleEndian.Uint64(data[32:40]),
+			binary.LittleEndian.Uint64(data[40:48]),
+			binary.LittleEndian.Uint64(data[48:56]),
+			binary.LittleEndian.Uint64(data[56:64]),
+		}
+	} else {
+		x = []uint64{
+			binary.BigEndian.Uint64(data[0:8]),
+			binary.BigEndian.Uint64(data[8:16]),
+			binary.BigEndian.Uint64(data[16:24]),
+			binary.BigEndian.Uint64(data[24:32]),
+			binary.BigEndian.Uint64(data[32:40]),
+			binary.BigEndian.Uint64(data[40:48]),
+			binary.BigEndian.Uint64(data[48:56]),
+			binary.BigEndian.Uint64(data[56:64]),
+		}
 	}
 
 	pass(&d.a, &d.b, &d.c, x, 5)
